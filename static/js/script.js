b2dbe30a40c580095a0c1431544cc5144a138a0e
Xavier L'Heureux <dev.xlheureux@gmail.com>
2021-11-07 12:28:51 -0500

    Block submit button while processing & add hidden input fields

--- static/js/script.js
+++ static/js/script.js
@@ -6,14 +6,16 @@ function onSubmit(e) {
   const form = document.getElementById('f_form');
   const values = new FormData(form);
   const vals = Object.fromEntries(values);
-  vals.Model = 'FitzHugh-Nagumo';
-  vals.InitialModel = 'Random';
-  vals.noise = 0.05;
-  vals.size = 100;
+  const button = document.getElementById('submit-button');
 
-  const keys = ['diffusionA', 'diffusionB', 'tau', 'k', 's', 'L', 'steps', 'noise', 'size']
+  button.disabled = true;
+  button.value = "Processing…";
+
+  const keys = ['diffusionA', 'diffusionB', 'tau', 'k', 's', 'L', 'steps', 'noise', 'size', 'Kill', 'Feed']
   for (key of keys) {
-    vals[key] = Number(vals[key]); // Convert to numbers
+    if (vals[key] !== undefined) {
+      vals[key] = Number(vals[key]); // Convert to numbers
+    }
   }
 
   const json = JSON.stringify(vals);
@@ -26,7 +28,11 @@ function onSubmit(e) {
     return resp.blob();
   }).then(blob => {
     document.getElementById('output').src = URL.createObjectURL(blob);
+    button.disabled = false;
+    button.value = "Generate";
   }).catch(e => {
+    button.disabled = true;
+    button.value = "Generate";
     alert(e);
   });
 }
