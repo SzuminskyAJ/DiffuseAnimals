9aaad532235deabfc130aab6637b1eaa3b9cd1e9
Yuliya Shpunarska <yuliya@pop-os.localdomain>
2021-11-07 11:48:24 -0500

    webapp

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,23 @@
 window.onload = () => {
     //alert('Tu pue');
 };
+
+function onSubmit(e) {
+  e.preventDefault();
+  const form = document.getElementById('f_form');
+  const values = new FormData(form);
+  const json = JSON.stringify(Object.fromEntries(values));
+
+  fetch('/simulation.gif', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: json
+  }).then(resp => {
+    if (!resp.ok) throw new Error('Could not process');
+    return resp.blob();
+  }).then(blob => {
+    myImage.src = URL.createObjectURL(blob);
+  }).catch(e => {
+    alert(e);
+  });
+}
